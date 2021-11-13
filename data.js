56e5fe8bff84917d6e5b7efdba1f1c2e1640917e
waldir <waldirescobar2018@gmail.com>
2021-11-13 11:31:47 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,33 @@
+let userid = "4283734956272812901"; 
+let apikey = "AIzaSyD7yjlETy2z-Fu4-pJ27QInEvjkgncgFpY"; 
+
+export { userid, apikey };
+
+/*
+81005315366-e7th8o1h32qh20s2kjn3r1svocc32m64.apps.googleusercontent.com
+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
+
+{
+  "kind": "blogger#page",
+  "id": "7758601029016984341",
+  "status": "DRAFT",
+  "blog": {
+    "id": "4283734956272812901"
+  },
+  "published": "2021-11-12T07:35:00-08:00",
+  "updated": "2021-11-12T07:35:03-08:00",
+  "url": "http://lovemesayuri.blogspot.com/",
+  "selfLink": "https://content-blogger.googleapis.com/v3/blogs/4283734956272812901/pages/7758601029016984341",
+  "title": "",
+  "content": "",
+  "author": {
+    "id": "16859022767205344799",
+    "displayName": "waldir",
+    "url": "https://www.blogger.com/profile/16859022767205344799",
+    "image": {
+      "url": "//www.blogger.com/img/blogger_logo_round_35.png"
+    }
+  },
+  "etag": "\"dGltZXN0YW1wOiAxNjM2NzMxMzAzOTUxCm9mZnNldDogLTI4ODAwMDAwCg\""
+}
+*/
